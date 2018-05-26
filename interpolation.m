4322c5d2a7f68148d01c218b5313b459981430c6
Gabriel Haggstrom <gshaggstrom@gmail.com>
2018-05-25 23:12:46 -0300

    Refatora interpolation.

--- interpolation.m
+++ interpolation.m
@@ -1,8 +1,18 @@
 function P = interpolation(I, x, y, method)
   switch lower(method)
       case 'replication'
-          P = interpolation_replication(I, x, y);
+          interp_function = @interpolation_replication;
       otherwise
           error("Unknown interpolation method " + method + ".")
+  end
+  
+  [N, M, numChannels] = size(I);
+  P = zeros(1, 1, numChannels);
+  for c = 1:numChannels
+      if x > 0 && x <= M && y > 0 && y <= N
+          P(c) = interp_function(I, x, y, c);
+      else
+          P(c) = 0;
+      end
   end
 end
